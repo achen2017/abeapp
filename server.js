c890846bff9afde165763eb24b54e7a4b1f2052f
achen2017 <achen2017>
2017-07-10 14:51:15 -0500

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+//To Change when installing for a new handle: webhook_url needs to be updated for the new app
+//So does the arguements in the if statement -----> req.body.event.text.includes(????)
+//Additionally you need to perform a digital handshake with each new app. This occurs with Slack's Events API
+
+
 //require the express nodejs module
 var express = require('express'),
 	//set an instance of exress
@@ -6,33 +11,70 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	//require the path nodejs module
 	path = require("path");
-	
+
+//this is the package the allows us to use webhooks to
+var Slack = require('slack-node');
+var webhook_url = 'https://hooks.slack.com/services/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';
+slack = new Slack();
+slack.setWebhook(webhook_url);
+
+
 //support parsing of application/json type post data
 app.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //tell express that www is the root of our public web folder
 app.use(express.static(path.join(__dirname, 'www')));
 
-//tell express what to do when the /form route is requested
-app.post('/form',function(req, res){
-	res.setHeader('Content-Type', 'application/json');
 
-	//mimic a slow network connection
-	setTimeout(function(){
+app.post('/',function(req, res){
 
-		res.send(JSON.stringify({
-			firstName: req.body.firstName || null,
-			lastName: req.body.lastName || null
-		}));
+	//This if tests if the incoming event is the URL verification handshake.
+	if (req.body.type == "url_verification") {
+		res.setHeader('Content-Type', 'text');
+		console.log(req.body.challenge);
+			res.writeHead(200, {"Content-Type": "application/x-www-form-urlencoded"});
+			  var otherArray = req.body.challenge;
+			  var json = JSON.stringify({
+			    body: otherArray,
+			  });
+				res.end(json);
+	} // end of verification if
 
-	}, 1000)
+	//verifies that the event type is a message
+	else if (req.body.event.type == "message") {
+		console.log("Oh! I got a message");
+		if (req.body.event.text.includes("abe") || req.body.event.text.includes("goat") || req.body.event.text.includes("abraham") || req.body.event.text.includes("banana")) {
+			res.writeHead(200, {"Content-Type": "application/json"});
 
-	//debugging output for the terminal
-	console.log('you posted: First Name: ' + req.body.firstName + ', Last Name: ' + req.body.lastName);
+			// console.log("whats?");
+
+
+
+
+			//this is the code that sends something back
+			slack.webhook({mrkdown: false, text: '*Abe* is ALWAYS listening...'}, function (err, response) {
+					// console.log(response);
+				});
+
+
+			console.log("got to the end");
+		}
+		res.end();
+	} // end of message if
+	else {
+		res.writeHead(200, {"Content-Type": "application/x-www-form-urlencoded"});
+		console.log("ops, I don't know this event type.");
+		console.log(req);
+		res.end();
+	}
+
+
 });
+
+
 
 //wait for a connection
 app.listen(3000, function () {
